ee36d7f387c8251b76d5ecec7c40a069c6853d99
Vladimir Guzman <vguzman@yahoo.com>
2013-07-17 03:08:10 +0000

    Homework 3, part 3

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var util = require('util');
 
 var assertFileExists = function(infile) {
 	var instr = infile.toString();
@@ -61,14 +63,51 @@ var clone = function(fn) {
 	return fn.bind({});
 };
 
+var checkURL = function(result, response) {
+	if (result instanceof Error) {
+		console.log("Error: " + util.format(result.code));
+		process.exit(1);
+	} else {
+		$ = cheerio.load(result);
+		var checksfile=program.checks;
+		var checks = loadChecks(checksfile).sort();
+		var out = {};
+		for(var ii in checks) {
+			var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+		}
+		var outJson = JSON.stringify(out, null, 4);
+		console.log(outJson);
+		process.exit(1);
+	}
+}
+
 if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to index.html')
+		.option('-u, --url <url>', 'URL to index.html')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	if(program.file!=undefined){
+		assertFileExists(program.file);
+		if(program.url!=undefined){
+			console.log("Select either --file or --url, not both. Exiting.");
+			process.exit(1);
+		}
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
+	else{//program.file==undefined
+		if(program.url==undefined){
+			console.log("Select either --file or --url. Exiting.");
+			process.exit(1);
+		}
+		rest.get(program.url).on('complete', checkURL);
+//		console.log(checkJson);
+//		process.exit(1);
+	}
+
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
